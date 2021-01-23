cded7c1957eefe67bbfc25d1a073ec229538e815
Chris Mulholland <chrismulholland@nypl.org>
2021-01-23 15:03:34 -0500

    1

---
+++ spec/z_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper.rb"
+require "support/capybara.rb"
+require "support/helpers.rb"
+require "csv"
+
+#get the Helpers module
+RSpec.configure do |c|
+  c.include Helpers
+  c.include Capybara::DSL
+  #c.fail_fast = true
+end
+
+describe "Just visits" do
+  before(:each) do
+    #sleep 2
+  end
+  after(:each) do
+    #sleep 1
+  end
+  it "visits Google and expect to see the word Gmail" do
+    visit "https://www.google.com"
+    expect(page).to have_content("Gmail")
+  end
+  
+end
